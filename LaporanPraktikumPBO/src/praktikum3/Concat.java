b4097e88fba53ad0832243ac5c2f3f61509bba7d
ahmaddedy <setiabudi19@gmail.com>
2015-06-28 16:13:52 +0800

    laporan praktikum 3

---
+++ LaporanPraktikumPBO/src/praktikum3/Concat.java
@@ -0,0 +1,10 @@
+package praktikum3;
+
+public class Concat {
+	public static void main(String[] args) {
+		String string1 = "Nama Saya";
+		String string2 = " Nureksanita";
+		String string3 = string1 + string2;
+		System.out.println(string3);
+	}
+}
---
+++ LaporanPraktikumPBO/src/praktikum3/PrePostDemo.java
@@ -0,0 +1,14 @@
+package praktikum3;
+
+public class PrePostDemo {
+	public static void main(String[] args) {
+		int i = 3;
+		i++;
+		System.out.println(i);//"4"
+		++i;
+		System.out.println(i);
+		System.out.println(++i);
+		System.out.println(i++);
+		System.out.println(i);
+	}
+}
