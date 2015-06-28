761a111dba87e5afe9649e3fa96adc6094ba4b84
ahmaddedy <setiabudi19@gmail.com>
2015-06-28 16:21:55 +0800

    laporan praktikum 4

---
+++ LaporanPraktikumPBO/src/praktikum4/ForDemo.java
@@ -0,0 +1,16 @@
+package praktikum4;
+
+public class ForDemo {
+	public static void main(String[] args) {
+		for (int i = 1; i<11; i++){
+			System.out.println("for 1: "+i);
+		}
+		for (int i = 10; i>=-3; i--){
+			System.out.println("for 1: "+i);
+		}
+		for (int i = 10, j=2; i>=-3 & j<10;i--, i++){
+			System.out.println("for 3: "+i+" "+j);
+		}
+		
+	}
+}
